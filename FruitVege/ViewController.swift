f7bb3b0534c9ee7c7ad1a7e42f8d1054d1e2eb87
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-06-23 17:40:56 +0900

    dekita

--- FruitVege/ViewController.swift
+++ FruitVege/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSavedText()
         // Do any additional setup after loading the view.
     }
     
